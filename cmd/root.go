93954ebafa7db838dd2a8caa1a159490bbf4363b
astro <astro@WorkStation.myguest.virtualbox.org>
2023-10-23 16:59:44 +0300

    Changes:\n- added be logic for get_workspace_id function\n- Modified project structure\n- Added some sample responses for for get_workspace_id requests

--- cmd/root.go
+++ cmd/root.go
@@ -10,7 +10,7 @@ import (
 )
 
 // rootCmd represents the base command when called without any subcommands
-var rootCmd = &cobra.Command{
+var RootCmd = &cobra.Command{
 	Use:   "orb",
 	Short: "A brief description of your application",
 	Long: `A longer description that spans multiple lines and likely contains
@@ -27,7 +27,7 @@ to quickly create a Cobra application.`,
 // Execute adds all child commands to the root command and sets flags appropriately.
 // This is called by main.main(). It only needs to happen once to the rootCmd.
 func Execute() {
-	err := rootCmd.Execute()
+	err := RootCmd.Execute()
 	if err != nil {
 		os.Exit(1)
 	}
